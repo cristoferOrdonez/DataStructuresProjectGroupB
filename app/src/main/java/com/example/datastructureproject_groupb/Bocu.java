f89a324d97f9f3ebc7b71badbddd3ee4b1e87aba
Anbeld <147835151+Anbeld@users.noreply.github.com>
2024-04-26 18:54:45 -0500

    Cree las activitys principales y los botones, tambien la app solo es vertical y el tema es balnco

--- app/src/main/java/com/example/datastructureproject_groupb/Bocu.java
+++ app/src/main/java/com/example/datastructureproject_groupb/Bocu.java
@@ -20,7 +20,7 @@ public class Bocu extends Application {
     public static DynamicUnsortedList<UsuarioComun> usuariosComunes;
     public static UsuarioRegistrado usuario;
     public static int estadoUsuario = SIN_REGISTRAR;
-    public static final String [] LOCALIDADES = new String[]{ "Virtual","Usaquén", "Chapinero", "Santa Fe", "San Cristóbal", "Usme", "Tunjuelito", "Bosa", "Kennedy", "Fontibón", "Engativá", "Suba", "Barrios Unidos", "Teusaquillo", "Los Mártires", "Antonio Nariño", "Puente Aranda", "La Candelaria", "Rafael Uribe Uribe", "Ciudad Bolívar", "Sumapaz"   };
+    public static final String [] LOCALIDADES = new String[]{"Usaquén", "Chapinero", "Santa Fe", "San Cristóbal", "Usme", "Tunjuelito", "Bosa", "Kennedy", "Fontibón", "Engativá", "Suba", "Barrios Unidos", "Teusaquillo", "Los Mártires", "Antonio Nariño", "Puente Aranda", "La Candelaria", "Rafael Uribe Uribe", "Ciudad Bolívar", "Sumapaz"   };
 
     public static final String [] INTERESES = new String[]{"Musica", "Talleres"};
     public static final String [] PLATAFORMAS = new String[]{"Discord", "Meet", "Skype", "Teems", "Zoom"};
